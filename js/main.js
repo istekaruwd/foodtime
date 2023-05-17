b5738cc50e501944604bb30a161a81e855f25268
Md. Istekar Islam <istekaruwd@gmail.com>
2023-05-17 07:30:36 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,37 @@
+/*
+Template Name: 
+Template URI: 
+Author: CoderHut
+Author URI: 
+Version: 1.0
+Tags: 
+*/
+
+(function ($) {
+
+    'use strict';
+
+/*  ======================================
+    Mobile Nav
+    ====================================== */
+
+    $(function(){
+		$('#menu').slicknav({
+            prependTo:'.mobile-nav',
+            label: ''
+    });
+	});
+    
+    
+    
+    $(window).on('load', function () {
+        
+    /*  ======================================
+        Preloader
+        ====================================== */
+        
+        
+        
+    });
+
+}(jQuery));
